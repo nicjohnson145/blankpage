19b0c9c6b70713faa6cb544d55113216c483612e
Nic Johnson <nicjohnson145@hotmail.com>
2026-02-12 12:52:45 -0600

    feat: taskfile & CLI improvements for bench testing

---
+++ cmd/cli/purge.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"connectrpc.com/connect"
+	blankpagev1 "github.com/nicjohnson145/blankpage/gen/go/blankpage/v1"
+	blankpagev1connect "github.com/nicjohnson145/blankpage/gen/go/blankpage/v1/blankpagev1connect"
+	pauthv1beta1 "github.com/nicjohnson145/blankpage/gen/go/pauth/v1beta1"
+	pauthv1beta1connect "github.com/nicjohnson145/blankpage/gen/go/pauth/v1beta1/pauthv1beta1connect"
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+)
+
+func Purge() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "purge",
+		Short: "purge the service (DANGER!)",
+		Long:  "Hit the /Purge endpoint for both auth & core objects, this endpoint may not be enabled depending on service configuration",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return withLogging(cmd, func(logger zerolog.Logger) error {
+				return withBlankpageClient(func(blankpageClient blankpagev1connect.BlankPageServiceClient) error {
+					return withPAuthClient(func(pauthClient pauthv1beta1connect.PAuthServiceClient) error {
+						ctx, cancel := context.WithCancel(context.Background())
+						defer cancel()
+
+						logger.Info().Msg("purging core objects")
+						if _, err := blankpageClient.Purge(ctx, connect.NewRequest(&blankpagev1.PurgeRequest{})); err != nil {
+							return fmt.Errorf("error purging core objects: %w", err)
+						}
+
+						logger.Info().Msg("purging auth")
+						if _, err := pauthClient.Purge(ctx, connect.NewRequest(&pauthv1beta1.PurgeRequest{})); err != nil {
+							return fmt.Errorf("error purging core objects: %w", err)
+						}
+
+						return nil
+					})
+				})
+			})
+		},
+	}
+
+	return cmd
+}
--- cmd/cli/util.go
+++ cmd/cli/util.go
@@ -7,11 +7,21 @@ import (
 	"time"
 
 	"connectrpc.com/connect"
+	blankpagev1connect "github.com/nicjohnson145/blankpage/gen/go/blankpage/v1/blankpagev1connect"
 	pauthv1beta1 "github.com/nicjohnson145/blankpage/gen/go/pauth/v1beta1"
 	pauthv1beta1connect "github.com/nicjohnson145/blankpage/gen/go/pauth/v1beta1/pauthv1beta1connect"
 	"github.com/nicjohnson145/blankpage/internal/cliconfig"
+	"github.com/nicjohnson145/blankpage/internal/logging"
 	"github.com/nicjohnson145/hlp"
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+)
+
+var (
+	baseHttpClient = &http.Client{
+		Timeout: 5 * time.Second,
+	}
 )
 
 // This function is not suitable for long-lived (i.e as long as a token duration) connections
@@ -52,4 +62,32 @@ func getTokenInterceptor(httpClient *http.Client) connect.UnaryInterceptorFunc {
 			return next(ctx, req)
 		}
 	}
+}
+
+func withLogging(cmd *cobra.Command, workFunc func(logger zerolog.Logger) error) error {
+	return workFunc(logging.Init(&logging.LoggingConfig{
+		// We've safely validated this already
+		Level:  hlp.Must(logging.ParseLogLevel(hlp.Must(cmd.Flags().GetString(cliconfig.FlagLogLevel)))),
+		Format: logging.LogFormatHuman,
+	}))
+}
+
+func withBlankpageClient(workFunc func(client blankpagev1connect.BlankPageServiceClient) error) error {
+	return workFunc(blankpagev1connect.NewBlankPageServiceClient(
+		baseHttpClient,
+		viper.GetString(cliconfig.FlagServerUrl),
+		connect.WithInterceptors(
+			getTokenInterceptor(baseHttpClient),
+		),
+	))
+}
+
+func withPAuthClient(workFunc func(client pauthv1beta1connect.PAuthServiceClient) error) error {
+	return workFunc(pauthv1beta1connect.NewPAuthServiceClient(
+		baseHttpClient,
+		viper.GetString(cliconfig.FlagServerUrl),
+		connect.WithInterceptors(
+			getTokenInterceptor(baseHttpClient),
+		),
+	))
 }
